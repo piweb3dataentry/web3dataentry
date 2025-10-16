65af10c773ee761b4baa5a68549a5da5aa17ac2e
Ajgar Ali molla <ajgaralimolla12@gmail.com>
2025-10-16 11:42:52 +0530

    Create User model with schema and export

---
+++ models/User.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  walletAddress: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  fullName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export default mongoose.model("User", userSchema);
